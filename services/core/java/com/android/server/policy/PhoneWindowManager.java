c9d553d0a7917cb33deb10af4d8fde2031368a49
D. Andrei Măceș <andrei@unlegacy-android.org>
2018-10-28 12:05:50 +0200

    wm: Add prop for home button wakeup
    Toggle persist.wm.home_wake_screen off to disable waking screen with HW
    key. The existing Settings plumbing is left in place, and would over-
    ride prop should the remaining UI bits be implemented.
    
    Change-Id: Ie06899b2e5433688e9a8f67c50815ede9da40405
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2483,7 +2483,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
 
             mHomeWakeScreen = (Settings.System.getIntForUser(resolver,
-                    Settings.System.HOME_WAKE_SCREEN, 1, UserHandle.USER_CURRENT) == 1);
+                    Settings.System.HOME_WAKE_SCREEN, SystemProperties.getBoolean(
+                    "persist.wm." + Settings.System.HOME_WAKE_SCREEN,
+                    true) ? 1 : 0, UserHandle.USER_CURRENT) == 1);
 
             // Configure wake gesture.
             boolean wakeGestureEnabledSetting = Settings.Secure.getIntForUser(resolver,
